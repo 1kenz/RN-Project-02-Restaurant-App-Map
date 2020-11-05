54e2be6a2e48a59fb3f0ebe3a08b4383a9861dd7
xkendx <63884610+xkendx@users.noreply.github.com>
2020-11-05 22:29:24 +0300

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import Axios from 'axios';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, View, Text, FlatList} from 'react-native';
 import MapView from 'react-native-maps';
 import {City, RestaurantDetail, SearchBar} from './components';
 
+let originalList = [];
+
 const Main = (props) => {
+  const [cityList, setCityList] = useState([]);
+
+  const fetchCities = async () => {
+    const {data} = await Axios.get(
+      'https://opentable.herokuapp.com/api/cities',
+    );
+    setCityList(data.cities);
+    originalList = [...data.cities];
+  };
+
+  useEffect(() => {
+    fetchCities();
+  }, []);
+
+  const onCitySearch = (text) => {
+    const filteredList = originalList.filter(item => {
+      const userText = text.toUpperCase();
+      const cityName = item.toUpperCase();
+
+      return cityName.indexOf(userText) > -1;
+    })
+
+    setCityList(filteredList);
+  };
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={{flex: 1}}>
@@ -17,7 +45,14 @@ const Main = (props) => {
           }}
         />
         <View style={{position: 'absolute'}}>
-          <SearchBar />
+          <SearchBar onSearch={onCitySearch} />
+          <FlatList
+            showsHorizontalScrollIndicator={false}
+            horizontal
+            keyExtractor={(_, index) => index.toString()}
+            data={cityList}
+            renderItem={({item}) => <City cityName={item} />}
+          />
         </View>
       </View>
     </SafeAreaView>
